27ec057c467d487e74327f8acb9b85f67b8037ee
Muhammad Khubaib <khubaib.mk@gmail.com>
2022-12-15 18:31:41 +0500

    Mixed array and union are explained also, defining empty array

--- step08_arrays/app.ts
+++ step08_arrays/app.ts
@@ -8,6 +8,21 @@ let array4: number[] = new number[2];//error
 let array5: number[] = [];
 array5.push(1234);//dynamically adding
 
+// Mixed array
+let array6: (number | string)[] = [];
+array6 = [1, 2, 3, 4];
+array6 = ["This", "is", "a", "string"];
+array6 = [1, 2, 3, 4, "This", "is", "a", "string"];
 
+// Union
+let array7: number[] | string[]; //This array can either be array of number or array of string
+array7 = [1, 2, 3, 4];
+array7 = ["Sir", "Zia"];
+// array7 = [1,2,3, "Sir", "Zial"]; // Typescript will complain and won't allow it.
+
+// Defining Array
+let array8: [] = []; //But this array will always be empty, it can never have anything.
+// array8 = [1, 2, 3];
+// array8 = ["sir", "zia"]; //Typescript won't allow it, as at type specification, it was infered as an empty array.
 
 
